5835f575b231a502e6e1b1e1545ce47a81151641
Irayya-Hiremath <ihiremath1991@gmail.com>
2021-05-21 23:06:23 +0530

    Intial commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Navbar(){
+    return(
+        <nav>
+             <a href="#" className="logo">
+                 UI Expert
+             </a>
+             <ul>
+                 <li><a href="#">About</a></li>
+                 <li><a href="#">Experiance</a></li>
+                 <li><a href="#">Skills</a></li>
+                 <li><a href="#">Education</a></li>
+                 <li><a href="#">Contact</a></li>
+
+             </ul>
+        </nav>
+    )
+}
+export default Navbar;
