6ab23ad3855fd4281a28ad38b28289355d148ff2
Irayya-Hiremath <ihiremath1991@gmail.com>
2021-05-23 22:41:55 +0530

    Creating components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,19 @@
 import React from "react"
+import Navbar from './Navbar/Navbar'
 
 function Header(){
     return(
+        <div className='main'>
+            <Navbar/>
+            <div className='name'>
+                <p>Looking For A Devloper</p>
+                <h1>I'm <span>Irayya</span></h1>
+                <p className="details">hhohsoasohhoohohoohohdahohohdaohohod</p>
+                <div className="header-btns">
+
+                </div>
+            </div>
+        </div>
     
     )
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,14 +6,19 @@ function Navbar(){
              <a href="#" className="logo">
                  UI Expert
              </a>
+             <input className='menu-btn' type='checkbox'/>
+             <label className="menu-icon" for="menu-btn">
+                 <span className='nav-icon'></span>
+             </label>
+
              <ul className="menu">
                  <li><a href="#">About</a></li>
                  <li><a href="#">Experiance</a></li>
                  <li><a href="#">Skills</a></li>
                  <li><a href="#">Education</a></li>
                  <li><a href="#">Contact</a></li>
-
              </ul>
+             <a href="#" className="Hey">Hey</a>
         </nav>
         
     )
