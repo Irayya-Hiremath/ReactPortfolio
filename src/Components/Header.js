0315095e1441d0c5a462e5fba65ab8ec47c2e148
Irayya-Hiremath <ihiremath1991@gmail.com>
2021-05-27 23:35:38 +0530

    Intial commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,29 +5,27 @@ function Header(){
     return(
         <div className='main'>
             <Navbar/>
-            <div className='name'>
-                <p className="heading">Hello</p>
-                <h1>I'm </h1>
+            
+                <h1 className="hello">Hello,</h1>
+                <h1 className="am">I am</h1>
 
+                
                 <about className="about">
                     <h1>
                         IR<span>AY</span>YA
                     </h1>
                 </about>
+                <detail className="detail">
+                <p >Irayya Hiremath, a Mechanical Engineering graduate from Hulkoti, Gadag. Started as a Design Engineer at BLH H-Tech PVT LTD, Hubli in an IOT team for SPOUT project. A Prototype development of an Alcohol inventory system developed using IOT technology, I was responsible for parts designing of Spout using Catia V5.
+                After completing the SPOUT prototype,<br/> Continued in the same organization as a customer facing Project Coordinator/Technical consultant for web and mobile Applications Under IT Dept. In that role I was responsible for gathering Customer requirements and signoff, Designing the wireframes and application flows.
+                Collaborating with Sales, development and testing team to complete the application implementation. Customer Demo and Presentation.
+                <br/>Currently,I am a Web developer intern at Sathvik Softech, Hubli. My responsibility include designing and developing UI for the websites and integrating it with back-end system.</p>
+
+                </detail>
 
                  
-                <p className="details">Irayya Hiremath, a Mechanical Engineering graduate from Hulkoti, Gadag. Started as a Design Engineer at BLH H-Tech PVT LTD, Hubli in an IOT team for SPOUT project. A Prototype development of an Alcohol inventory system developed using IOT technology, I was responsible for parts designing of Spout using Catia V5.
-                After completing the SPOUT prototype, Continued in the same organization as a customer facing Project Coordinator/Technical consultant for web and mobile Applications Under IT Dept. In that role I was responsible for gathering Customer requirements and signoff, Designing the wireframes and application flows. Collaborating with Sales, development and testing team to complete the application implementation. Customer Demo and Presentation.
-                Currently, I am a Web developer intern at Sathvik Softech, Hubli. My responsibility include designing and developing UI for the websites and integrating it with back-end system.</p>
-               
-                {/* <div className="header-btns">
-                    <a href="#" className="btn-1">Hire ME</a>
-                    <a href="#" className="btn-2">Download CV</a>
-
-
-
-                </div> */}
-            </div>
+                
+            
         </div>
     
     )
