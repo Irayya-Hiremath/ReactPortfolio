86566dea1464080fe55cfbb55e59d64d72be014e
Irayya-Hiremath <ihiremath1991@gmail.com>
2021-05-28 23:59:09 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react"
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header';
+import Experiance from "./Components/Experiance"
 function App() {
   return (
     <>
+      <Navbar/>
       <Header/>
+      <Experiance/>
   
       
     </>
--- src/Components/Experiance.js
+++ src/Components/Experiance.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Experiance(){
     return(
-        <div>
+        <div className="exp">
+            <div className="exp-Tabs">
+                <div className="tab1">
+                    <div className="front"> </div>
+                    <div className="back"> </div>
+
+
+                </div>
+
+            </div>
             
         </div>
     )
