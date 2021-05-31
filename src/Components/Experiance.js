b8585a6fd5caada7d890e2b66cc283a99792fad0
Irayya-Hiremath <ihiremath1991@gmail.com>
2021-05-31 23:13:39 +0530

    Intial commit

--- src/Components/Experiance.js
+++ src/Components/Experiance.js
@@ -5,8 +5,20 @@ function Experiance(){
         <div className="exp" id="Exp">
             <div className="exp-Tabs">
                 <div className="tab1">
-                    <div className="front"> </div>
-                    <div className="back"> </div>
+                    <div className="front"> 
+                    <h1>SATHVIK SOFTECH</h1>
+                    <h2>Web Development Intern</h2>
+                    </div>
+                    <div className="back"> 
+                    <p>
+                    • Portfolio
+                            Design And developed My Portfolio by using HTML, Tailwind CSS& JavaScript Technology
+                    • Race Game
+                            Design And developed game by using HTML, CSS& JavaScript Technology
+                    • Number Guessing Game
+                            A random number will generate we have to guess the that the no. is odd or even. Design And developed game by using HTML, CSS& JavaScript Technology .
+                    </p>
+                    </div>
 
 
                 </div>
