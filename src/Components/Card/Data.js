0416d0245a2b26c576e39eea6f2c916a5efccd6c
Irayya-Hiremath <ihiremath1991@gmail.com>
2021-06-02 00:55:39 +0530

    Intial commit

--- src/Components/Card/Data.js
+++ src/Components/Card/Data.js
@@ -1,11 +1,10 @@
-const Data ={
-    company:"SATHVIKSOFTECH",
-    role:"Intern",
-    expe:"Portfolio Design And developed My Portfolio by using HTML, Tailwind CSS&JavaScript Technology Race Game Design And developed game by using HTML, CSS&JavaScript Technology Number Guessing GameA random number will generate we have to guess the that the no. is odd or even. Design And developed game by using HTML, CSS& JavaScript Technology "
+const Data =
+[
+    {
+    scompany:"SATHVIKSOFTECH",
+    srole:"Intern",
+    sexpe:"PortfCSS& JavaScript Technology ",
 },
+]
 
-{
-    company:"BLH PVT LTD",
-    role:"Design Engineer",
-    expe:"Portfolio Design And  My Portfolio by using HTML, Tailwind CSS&JavaScript Technology Race Game Design And developed game by using HTML, CSS&JavaScript Technology Number Guessing GameA random number will generate we have to guess the that the no. is odd or even. Design And developed game by using HTML, CSS& JavaScript Technology"
-},
+export default Data;
--- src/Components/Experiance.js
+++ src/Components/Experiance.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import Card from "./Card/Card"
+import Data from "./Card/Data";
 
 function Experiance(){
     return(
         <div className="exp" id="Exp">
             <div className="exp-Tabs">
-                <div className="tab1">
+                <Card
+                scompany={Data[0].scompany} 
+                srole={Data[0].srole}
+                sexpe={Data[0].sexpe}/>
+
+<Card
+                scompany={Data[0].scompany} 
+                srole={Data[0].srole}
+                sexpe={Data[0].sexpe}/>
+                {/* <div className="tab1">
                     <div className="front"> 
                     <h1>SATHVIK SOFTECH</h1>
                     <h2>Web Development Intern</h2>
+                    <h1>{props.company}</h1>
+                    <h2>{props.role}</h2>                        
                     </div>
                     <div className="back"> 
+                    {props.expe}
                     <p>
                     <h3>Portfolio</h3>Design And developed My Portfolio by using HTML, Tailwind CSS&JavaScript Technology
                     <h3>Race Game</h3> Design And developed game by using HTML, CSS&JavaScript Technology
@@ -18,7 +32,7 @@ function Experiance(){
                     </div>
 
 
-                </div>
+                </div> */}
 
             </div>
             
