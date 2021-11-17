d27ea146b550aaa8abc3bb9fa37e43cd5cb971b8
irayya <ihiremath1991@gmail.com>
2021-11-17 23:07:27 +0530

    new portfolio design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavMenu from '../Components/NavMenu'
 
 function Home() {
     return (
         <div>
+            <NavMenu/>
             <h1>home  page</h1>
         </div>
     )
